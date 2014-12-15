eefb263913bc728a7e9e432bbffa8ace9ed0407b
Mike Borthwick <mike@strixy.com>
2014-12-14 20:01:02 -0700

    First edition of css and JS strip show

---
+++ ugly.php
@@ -0,0 +1,12 @@
+<?php 
+
+if($_GET['url'])
+{
+	$page = file_get_contents('http://'.$_GET['url']);
+	$page = preg_replace("/.*<body[^>]*>|<\/body>.*/si", "", $page);
+	$search = array ("'<script[^>]*?>.*?</script>'si","'<style[^>]*?>.*?</style>'si","'<link[^>]*?>.*?</link>'si","'<object[^>]*?>.*?</object>'si");
+	$replace = array ("","","","","");
+	$site = preg_replace ($search, $replace, $page);
+	echo $site;
+}
+

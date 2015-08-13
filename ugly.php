6bec64dfee474034365e138d6fe52e6a57f1bec9
Strixy <mike@strixy.com>
2015-08-12 22:27:27 -0600

    Quick fix to help those cases where a redirect from http to https isn't configured or when someone specifically adds http or https to the url. This still doesn't address all cases, but at least it's a little more rigorous to the most common user inputs not covered by the primary use case.

--- ugly.php
+++ ugly.php
@@ -2,9 +2,29 @@
 
 if($_GET['url'])
 {
-	$page = file_get_contents('http://'.$_GET['url']);
+	$resource = "http://";
+
+	if(stristr($_GET['url'],"https"))
+	{
+		$resource = "https://";
+	}
+
+	$url = str_replace(["https://","http://"],'',$_GET['url']);
+	$page = file_get_contents($resource.$url);
+	if((!stristr($page,"<head>") || !stristr($page,"<body")) && $resource == "https://")
+	{
+		$page = file_get_contents("http://".$url);
+	}
+	if((!stristr($page,"<head>") || !stristr($page,"<body")) && $resource == "http://")
+	{
+		die('<span style="">Did you ask me to check a website or a bananna?</span>');
+	}
 	$search = array ("'<script[^>]*?>.*?</script>'si","'<style[^>]*?>.*?</style>'si","'<link[^>]*?>'si","'<object[^>]*?>.*?</object>'si");
 	$replace = array ("","","","","");
 	$site = preg_replace ($search, $replace, $page);
 	echo $site;
+}
+else 
+{
+	die('<span style="">Hey now, I can\'t do anything without a little direction.</span>');
 }
